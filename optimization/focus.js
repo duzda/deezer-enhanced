a0d608a4f981a4c7e55106a2214834c90204b878
duzda <25201406+duzda@users.noreply.github.com>
2021-09-29 11:33:06 +0200

    refactor: optimize app

--- optimization/focus.js
+++ optimization/focus.js
@@ -1,10 +1,12 @@
 var logo = document.getElementById("central_logo");
 var app = document.getElementById("dzr-app")
 
-if (logo) {
-    logo.style = "opacity: -1;transform: scale(0,0)";
-}
-if (app) {
-    app.style = "display: block;";
-    document.body.style = "overflow-y: scroll;";
-}
+setTimeout(() => {
+    if (logo) {
+        logo.style = "opacity: -1;transform: scale(0,0)";
+    }
+    if (app) {
+        app.style = "display: block;";
+        document.body.style = "overflow-y: scroll;";
+    }
+}, 100)
