aede3e0229e3890803d220125fc6d55ff0ba28f0
duzda <duzda666@gmail.com>
2022-07-27 18:09:25 +0200

    dont restart webserver when unchanched

--- src/window/hooks/web_server_hook.js
+++ src/window/hooks/web_server_hook.js
@@ -6,16 +6,22 @@ class WebServerHook extends IHook {
         super();
         this.func = this.setWebServer;
         this.callbackName = 'webPort';
+        this.lastPort = 0;
     }
 
     setWebServer() {
         if (this.settings.getAttribute(this.callbackName) != 0) {
             if (!this.window.webserver) {
-                this.window.webserver = new WebServer(this.window, this.settings.getAttribute('webPort'));
+                this.window.webserver = new WebServer(this.window, this.settings.getAttribute(this.callbackName));
             } else {
+                if (this.lastPort == this.settings.getAttribute(this.callbackName)) {
+                    return;
+                }
+
                 this.window.webserver.stop();
             }
 
+            this.lastPort = this.settings.getAttribute(this.callbackName);
             this.window.webserver.start(this.settings.getAttribute(this.callbackName));
         } else {
             if (!this.window.webserver) {
