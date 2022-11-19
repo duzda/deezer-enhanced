48c40fb53b37b7df6d2a5c69fa4177f1acb424cf
duzda <25201406+duzda@users.noreply.github.com>
2022-11-19 09:21:24 +0100

    steamdeck as fullscreen

--- src/controllers/window_bounds.js
+++ src/controllers/window_bounds.js
@@ -1,11 +1,12 @@
 const FileManager = require('../utils/file_manager');
 const { screen } = require('electron');
+const exec = require('child_process').exec;
 
 const filename = 'window_bounds.json';
 
 const defaults = {
     bounds: {
-        width: 1200,
+        width: 1280,
         height: 800
     },
     maximized: false
@@ -14,7 +15,27 @@ const defaults = {
 class WindowBounds extends FileManager {
     constructor(window) {
         super(filename, defaults);
+
+        const windowManager = exec('echo $XDG_CURRENT_DESKTOP');
+
+        windowManager.stdout.on('data', (response) => {
+            if (response == 'gamescope\n') {
+                this.isSteamDeck = true;
+            }
+        });
+
         this.onload = () => {
+            if (this.isSteamDeck) {
+                window.setBounds({
+                    x: 0,
+                    y: 0,
+                    width: 1280,
+                    height: 800,
+                });
+                window.maximize();
+                return;
+            }
+
             const allDisplaysSummaryWidth = screen
                 .getAllDisplays()
                 .reduce((accumulator, { size: { width } }) => accumulator + width, 0);
