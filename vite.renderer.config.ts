74443c9508daafb802d48cf53223a8c4011b3425
duzda <25201406+duzda@users.noreply.github.com>
2025-10-11 19:25:58 +0200

    cleanup: remove autoprefixer + postcss

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ]
+});
